a83330becc264393cd73d58e0deb3d6de922204d
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2023-08-14 11:49:19 +0100

    close btn added

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 import { menuArray } from "./data.js";
 
 const menuItems = document.getElementById("menu-items")
+const selectedItem = document.getElementById("selected-item")
+const closeOrderBtn = document.getElementById("close-order-btn")
+
+
+
 
 let menuItemHtml = ""
 menuArray.forEach(function(item){
@@ -12,8 +17,18 @@ menuArray.forEach(function(item){
             <p class="menu-item-description">${item.ingredients}</p>
             <h3 class="menu-item-price">€${item.price}</h3>
           </div>
-          <button class="add-btn" id="add-btn">+</button>
+          <button class="add-btn" id="add-btn" >+</button>
         </div>
   `
   menuItems.innerHTML += item
 })
+
+closeOrderBtn.addEventListener("click", function(){
+  selectedItem.style.display = "none"
+})
+
+document.getElementById("add-btn").addEventListener("click",function(){
+  selectedItem.style.display = "block"
+})
+
+
